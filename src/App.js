cb09b4b9d6751e2b6bcaec05a1c11df1b44287e8
Philip Wong <pmhwong@gmail.com>
2023-06-21 01:27:32 +0800

    Initial commit for login form

--- src/App.js
+++ src/App.js
@@ -22,19 +22,20 @@
 //   );
 // }
 
+import "bootstrap/dist/css/bootstrap.min.css"
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import Auth from "./Auth"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>Hello from V2</h1>
-      </header>
-    </div>
-  );
+    <BrowserRouter>
+      <Routes>
+        <Route path="/auth" element={<Auth />} />
+      </Routes>
+    </BrowserRouter>
+  )
 }
 
 export default App;
